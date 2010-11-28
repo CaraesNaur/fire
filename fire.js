79daed1dd4dd1eba1732d4a6aec8d6849a57d226
oem <darkoem@gmail.com>
2010-11-28 09:51:58 +0100

    Added Fire#interpolate

--- fire.js
+++ fire.js
@@ -5,21 +5,21 @@
 // -- move everything up a line
 // -- loop
 
-function get_random ( number ) {
-    return( parseInt( Math.random() * number ));
+function get_random (number) {
+    return (parseInt( Math.random() * number, 10));
 }
 
 function palette (index) {
-    return("rgb(" +index+ ", 0, 0)");
+    return ("rgb(" + index + ", 0, 0)");
 }
 
 // x and y will be swapped, since it makes manipulating the fieldrows much easier
 function init_canvas (x, y) {
-    canvas = new Array(y);
+    var canvas = new Array(y);
     for (var i = 0; i < y; i++) {
         canvas[i] = new Array(x);
     }
-    return(canvas);
+    return (canvas);
 }
 
 function random_heatspots () {
@@ -33,19 +33,32 @@ function move_up () {
     this.canvas.shift();
 }
 
-function Fire (context, max_x, max_y) {
-    this.max_x            = max_x;
-    this.max_y            = max_y;
-    this.context          = context;
-    this.canvas           = init_canvas(max_x, max_y);
-    //methods
-    this.random_heatspots = random_heatspots;
-    this.move_up          = move_up;
+function interpolate_point (x, y) {
+    var coords = [[x-1, y], [x+1, y], [x, y-1], [x, y+1], [x-1, y-1], [x-1, y+1], [x+1, y-1], [x+1, y+1]];
+    var color = 0;
+    var neighbours = 0;
+    for (var i = 0; i < coords.length; i++) {
+        if ((coords[i][0] >= 0) && (coords[i][0] < this.max_x) && (coords[i][1] >= 0) && (coords[i][1] < this.max_y)) {
+            color += this.canvas[coords[i][1]][coords[i][0]] || 0;
+            neighbours += 1;
+        }
+    }
+    return (parseInt(color / neighbours, 10));
 }
 
+function Fire (context, max_x, max_y) {
+    this.max_x             = max_x;
+    this.max_y             = max_y;
+    this.context           = context;
+    this.canvas            = init_canvas(max_x, max_y);
+    //methods
+    this.random_heatspots  = random_heatspots;
+    this.move_up           = move_up;
+    this.interpolate_point = interpolate_point;
+}
 
 window.onload = function () {
     var canvas = document.getElementById('my_canvas');
     var ctx = canvas.getContext("2d");
-}
+};
 
