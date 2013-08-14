9984e998011d2e81a3f55f533ca0749c5d063bf4
Marty Vance <marty.vance@gmail.com>
2013-08-13 18:41:18 -0600

    Refactored the Fire JS object to use prototype functions.
    Added a default palette of 100 RGB triplets.  Pass in another array as the 6th
    argument to the Fire constructor.
    
    Ensured that the canvas[y][x] values remain within the range 0 to the number of
    colors.
    
    Added burn() and out() functions to start/stop the fire.  The timeout is now a
    property of the Fire object.
    
    Added 7th Fire constructor argument, speed, used as the delay argument for the
    animation timeout.
    
    Changed a lot of what happens inside interpolate_point().  Cooling is no longer
    linear, but based on a sine curve: cold colors cool faster.  Three new fire
    properties (cf1, cf2, focus) affect how much a color cools.  The cf1 and cf2
    properties determine the random cooling range, focus sets how much influence
    the original color has in determining the cooled color.
    
    Moved the instantiation into the html file; fire.js is now just the object code.
    
    Put a new look and feel into the HTML/CSS.  Add a click handler to the canvas to
    allow the fire to be paused.

--- README.md
+++ README.md
@@ -1,5 +1,28 @@
 # Oldschool fire effect with javascript + html5 canvas
 
-You can see it in action over at [arachne](http://arachne.heroku.com/fire)
+Forked from https://github.com/oem/fire.
 
-Just poke me here on github if you'd like me to quickly explain the fire algorithm - it is dead simple actually.
+Refactored the Fire JS object to use prototype functions.
+
+Added a default palette of 100 RGB triplets.  Pass in another array as the 6th
+argument to the Fire constructor.
+
+Ensured that the canvas[y][x] values remain within the range 0 to the number of
+colors.
+
+Added burn() and out() functions to start/stop the fire.  The timeout is now a
+property of the Fire object.
+
+Added 7th Fire constructor argument, speed, used as the delay argument for the
+animation timeout.
+
+Changed a lot of what happens inside interpolate_point().  Cooling is no longer
+linear, but based on a sine curve: cold colors cool faster.  Three new fire
+properties (cf1, cf2, focus) affect how much a color cools.  The cf1 and cf2
+properties determine the random cooling range, focus sets how much influence
+the original color has in determining the cooled color.
+
+Moved the instantiation into the html file; fire.js is now just the object code.
+
+Put a new look and feel into the HTML/CSS.  Add a click handler to the canvas to
+allow the fire to be paused.
--- fire.js
+++ fire.js
@@ -1,108 +1,276 @@
-// oldschool fire effect by oem
-// Algorithm:
-// -- move everything up a line
-// -- create random line at the bottom
-// -- interpolate every pixel with the 8 surrouding pixels
-// -- loop
+/*
+ * oldschool fire effect by oem (modified by CaraesNaur)
+ * Algorithm:
+ * -- move everything up a line
+ * -- create random line at the bottom
+ * -- interpolate every pixel with the 4 surrouding edge-adjacent pixels
+ * -- repeat
+ *
+ *
+ *
+ */
 
-function get_random (number) {
-    return (parseInt( Math.random() * number, 10));
+function Fire (context, max_x, max_y, scale, heat, pal, speed) {
+	var s = parseInt(speed);
+
+	this.max_x             = max_x || 320;
+	this.max_y             = max_y || 200;
+	this.heat              = heat || 50;
+	this.scale             = scale || 1;
+	this.context           = context;
+	this.canvas            = this.init_canvas(this.max_x, this.max_y);
+	this.hotspots          = 0; // skip drawing the last x lines
+	this.cooldown          = 5;
+	this.colors            = (pal instanceof Array) ? pal : Fire.prototype._default_colors;
+	this.timeout           = null;
+	this.speed             = (s > 50) ? s : 50;
+
+	// these three properties control the appearance of the fire
+	// cf1 and cf2 affect the cooling rate
+	// focus controls how sharp/blurry the flame is
+	this.cf1 = .75;
+	this.cf2 = 5;
+	this.focus = 10;
+
+	// highlight row: set this to an int in the range 0..[this.max_y - 1]
+	// to have all other rows dimmed
+	this.hl = null;
 }
 
-function palette (index) {
-    // even better than this would be a precreated palette of course
-    return ("rgb(" + index + "," + parseInt(index / 3, 10) + "," + parseInt(index / 5) + ")");
+Fire.prototype._toRad = Math.PI / 180;
+
+Fire.prototype.burn = function (d) {
+	var f;
+
+	d = (typeof d == 'number' ? Math.max(50, parseInt(d)) : this.speed);
+
+	this.speed = d;
+
+	if (this.timeout != null) {
+		this.out();
+	}
+
+	f = this;
+
+	this.timeout = setInterval(function () {
+		f.loop();
+	}, d);
 }
 
-// x and y will be swapped, since it makes manipulating the fieldrows much easier
-function init_canvas (x, y) {
-    var canvas = new Array(y);
-    for (var i = 0; i < y; i++) {
-        canvas[i] = new Array(x);
-    }
-    return (canvas);
+Fire.prototype.out = function () {
+	clearTimeout(this.timeout);
+	this.timeout = null;
 }
 
-function random_heatspots () {
-    var heatspots = 0;
-    var x = 0;
-    while (heatspots < this.heat) {
-        x = get_random(this.canvas[0].length);
-        if (this.canvas[this.canvas.length - 1][x] != 255) {
-            this.canvas[this.canvas.length - 1][x] = 255;
-            heatspots += 1;
-        }
-    }
+Fire.prototype.random_heatspots = function () {
+	var heatspots = 0;
+	var x = 0;
+
+	while (heatspots < this.heat) {
+		x = (parseInt(Math.random() * this.max_x, 10));
+
+		if (this.canvas[this.canvas.length - 1][x] != (this.colors.length - 1)) {
+			this.canvas[this.canvas.length - 1][x] = this.colors.length;
+			heatspots++;
+		}
+	}
 }
 
-function move_up () {
-    this.canvas.push(new Array(this.max_x));
-    this.canvas.shift();
+Fire.prototype.move_up = function () {
+	var n = new Array(this.max_x);
+
+	for (var i = 0; i < this.max_x; i++) {
+		n[i] = 0;
+	}
+
+	this.canvas.push(n);
+	this.canvas.shift();
 }
 
-function interpolate_point (x, y) {
-    var coords = [[x-1, y], [x+1, y], [x, y-1], [x, y+1]];
-    var color = 0;
-    var neighbours = 0;
-    for (var i = 0; i < coords.length; i++) {
-        if ((coords[i][0] >= 0) && (coords[i][0] < this.max_x) && (coords[i][1] >= 0) && (coords[i][1] < this.max_y)) {
-            color += this.canvas[coords[i][1]][coords[i][0]] || 0;
-            neighbours += 1;
-        }
-    }
-    color -= this.cooldown;
-    return (parseInt(color / neighbours, 10));
+Fire.prototype.interpolate_point = function (x, y) {
+	var coords = [
+		[x - 1, y],
+		[x + 1, y],
+		[x, y - 1],
+		[x, y + 1]
+	];
+	var oc = Math.min(this.canvas[y][x], this.colors.length - 1);
+	var ocr = ((oc + 1) / this.colors.length) * .9;
+	var ocsin = 1 - Math.sin(ocr * this._toRad);
+	var color = oc * this.focus;
+	var neighbours = this.focus;
+
+	for (var i = 0; i < coords.length; i++) {
+		if ((coords[i][0] >= 0) && (coords[i][0] < this.max_x) && (coords[i][1] >= 0) && (coords[i][1] < this.max_y)) {
+			color += this.canvas[ coords[i][1] ][ coords[i][0] ] || 0;
+			neighbours++;
+		}
+	}
+
+	color /= neighbours;
+	cool = parseInt(ocsin * (Math.random() * (this.cf1 * this.cf2)));
+
+	if (color < 0) {
+		color = 0;
+	}
+
+	return (color == 0 ? 0 : (parseInt(Math.min(color, this.colors.length - 1), 10) - cool));
 }
 
-function interpolate_all () {
-    for (var x = 0; x < this.max_x; x++) {
-        for (var y = 0; y < this.max_y; y++) {
-            this.canvas[y][x] = this.interpolate_point(x, y);
-        }
-    }
+Fire.prototype.interpolate_all = function () {
+	for (var x = 0; x < this.max_x; x++) {
+		for (var y = 0; y < this.max_y; y++) {
+			this.canvas[y][x] = this.interpolate_point(x, y);
+		}
+	}
 }
 
-function draw_fire () {
-    for (var x = 0; x < this.max_x; x++) {
-        for (var y = 0; y < this.max_y - this.hotspots; y++) {
-            this.context.fillStyle = palette(this.canvas[y][x]);
-            this.context.fillRect(x*this.scale, y*this.scale, this.scale, this.scale);
-        }
-    }
+Fire.prototype.draw_fire = function () {
+	for (var x = 0; x < this.max_x; x++) {
+		for (var y = 0; y < this.max_y - this.hotspots; y++) {
+			this.context.fillStyle = this.palette(this.canvas[y][x], (typeof this.hl != 'number' || y === this.hl));
+			this.context.fillRect(x * this.scale, y * this.scale, this.scale, this.scale);
+		}
+	}
 }
 
-function loop () {
-    this.move_up();
-    this.random_heatspots();
-    this.interpolate_all();
-    this.draw_fire();
+Fire.prototype.loop = function () {
+	this.move_up();
+	this.random_heatspots();
+	this.interpolate_all();
+	this.draw_fire();
 }
 
-function Fire (context, max_x, max_y, scale, heat) {
-    this.max_x             = max_x || 320;
-    this.max_y             = max_y || 200;
-    this.heat              = heat || 50;
-    this.scale             = scale || 1;
-    this.context           = context;
-    this.canvas            = init_canvas(this.max_x, this.max_y);
-    this.hotspots          = 1; // skip drawing the last x lines
-    this.cooldown          = 6;
-    //methods
-    this.random_heatspots  = random_heatspots;
-    this.move_up           = move_up;
-    this.interpolate_point = interpolate_point;
-    this.interpolate_all   = interpolate_all;
-    this.draw_fire         = draw_fire;
-    this.loop              = loop;
-    this.palette           = palette;
+Fire.prototype.palette = function (index, b) {
+	if (typeof index != 'number') {
+		index = 0;
+	}
+
+	//make sure index in within the range 0..[this.colors.length - 1]
+	index = parseInt(Math.min(this.colors.length - 1, Math.max(index, 0)));
+
+	if (index >= this.colors.length) {
+		return ('rgb(' + this.colors[this.colors.length - 1].join(',') + ')');
+	}
+
+	return ('rgb(' + this.colors[index].join(',') + ')');
 }
 
-window.onload = function () {
-    var canvas = document.getElementById('my_canvas');
-    var ctx = canvas.getContext("2d");
-    var fire = new Fire(ctx, 60, 40, 10, 30);
-    fire.draw_fire();
-    fire_interval = setInterval(function () {
-            fire.loop();
-            }, 25);
-};
+Fire.prototype.init_canvas = function (x, y) {
+	// x and y will be swapped, since it makes manipulating the fieldrows much easier
+	// therefore, a given coordinate is at this.canvas[y][x]
+	var canvas = new Array(y);
+
+	for (var i = 0; i < y; i++) {
+		canvas[i] = new Array(x);
+
+		for (var j = 0; j < x; j++) {
+			canvas[i][j] = 0;
+		}
+	}
+
+	return (canvas);
+}
+
+Fire.prototype._default_colors = [
+	[0,0,0],
+	[5,0,0],
+	[10,0,0],
+	[14,0,0],
+	[19,0,0],
+	[23,0,0],
+	[27,0,0],
+	[31,0,0],
+	[35,0,0],
+	[39,0,0],
+	[43,0,0],
+	[47,0,0],
+	[51,0,0],
+	[55,0,0],
+	[59,0,0],
+	[63,0,0],
+	[67,0,0],
+	[71,0,0],
+	[75,0,0],
+	[79,0,0],
+	[83,0,0],
+	[88,0,0],
+	[92,0,0],
+	[96,0,0],
+	[100,0,0],
+	[104,0,0],
+	[108,0,0],
+	[112,0,0],
+	[116,0,0],
+	[121,0,0],
+	[125,0,0],
+	[129,0,0],
+	[133,0,0],
+	[137,0,0],
+	[141,0,0],
+	[145,0,0],
+	[149,0,0],
+	[153,0,0],
+	[157,0,0],
+	[161,0,0],
+	[165,0,0],
+	[169,0,0],
+	[173,0,0],
+	[177,0,0],
+	[181,0,0],
+	[185,0,0],
+	[190,0,0],
+	[194,0,0],
+	[198,0,0],
+	[202,0,0],
+	[205,2,0],
+	[207,6,0],
+	[209,10,0],
+	[211,14,0],
+	[213,18,0],
+	[215,22,0],
+	[217,26,0],
+	[219,30,0],
+	[221,35,0],
+	[223,39,0],
+	[225,43,0],
+	[227,47,0],
+	[229,51,0],
+	[231,55,0],
+	[233,59,0],
+	[235,63,0],
+	[237,67,0],
+	[239,71,0],
+	[241,75,0],
+	[243,79,0],
+	[245,83,0],
+	[248,88,0],
+	[250,92,0],
+	[252,96,0],
+	[254,100,0],
+	[255,105,1],
+	[255,111,3],
+	[255,117,5],
+	[255,123,7],
+	[255,130,9],
+	[255,136,11],
+	[255,142,13],
+	[255,148,15],
+	[255,154,17],
+	[255,160,19],
+	[255,166,21],
+	[255,172,23],
+	[255,179,25],
+	[255,185,27],
+	[255,191,29],
+	[255,197,31],
+	[255,203,33],
+	[255,209,35],
+	[255,215,37],
+	[255,221,39],
+	[255,227,41],
+	[255,234,44],
+	[255,240,46],
+	[255,246,48],
+	[255,252,50]
+];
